8b8887359ff86b3358b8500579ccd0e331e36a25
mstrusch <mstr1@asia.com>
2016-11-25 13:21:08 +0700

    Master

---
+++ code/index copy.php
@@ -0,0 +1,7 @@
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+?>
+<iframe class="giphy-embed" src="//giphy.com/embed/JliGmPEIgzGLe" width="480" height="267" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
